c2549c59f9a33c92600e4f7aa250e189f18342e4
Isaac Cooper <isaaccooper10@gmail.com>
2024-09-20 14:39:06 -0400

    12 random cards on page init

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -7,7 +7,7 @@ class Card {
   }
 
   imgname() {
-    return this.number + this.color + this.fill + this.shape;
+    return this.number + this.fill + this.color + this.shape;
   }
 }
 
@@ -47,98 +47,105 @@ cards.forEach((card) => {
   j++;
 });
 
-// Helper function to check if an attribute forms a valid set 
-function sameOrDifferent(attr1, attr2, attr3){
-  return (attr1 === attr2 && attr2 === attr3) || (attr1 !== attr2 && attr1 !== attr3 && attr2 !== attr3);
+// Helper function to check if an attribute forms a valid set
+function sameOrDifferent(attr1, attr2, attr3) {
+  return (
+    (attr1 === attr2 && attr2 === attr3) ||
+    (attr1 !== attr2 && attr1 !== attr3 && attr2 !== attr3)
+  );
 }
 
-// Combines these checks for all four attributes. 
+// Combines these checks for all four attributes.
 // If all attributes either match or are different in this way, the three cards form a valid set.
-function isSet(card1, card2, card3){
+function isSet(card1, card2, card3) {
   return (
-    sameOrDifferent(card1.number, card2.number, card3.number) && 
+    sameOrDifferent(card1.number, card2.number, card3.number) &&
     sameOrDifferent(card1.color, card2.color, card3.color) &&
     sameOrDifferent(card1.fill, card2.fill, card3.fill) &&
     sameOrDifferent(card1.shape, card2.shape, card3.shape)
-
-
   );
 }
 
 let timeRemaining = 60;
 let timerInterval;
 
-// Timer Implementation 
-function startTimer(){
+// Timer Implementation
+function startTimer() {
   timerInterval = setInterval(() => {
-    if(timeRemaining > 0){
+    if (timeRemaining > 0) {
       timeRemaining--;
-      // May need to be altered when HTML is added 
-      console.log('Time Remaining: ${timeRemaining} seconds');
-
-    }else{
-      // Stops Timer 
+      // May need to be altered when HTML is added
+      console.log("Time Remaining: ${timeRemaining} seconds");
+    } else {
+      // Stops Timer
       clearInterval(timerInterval);
-      // // May need to be altered when HTML is added 
+      // // May need to be altered when HTML is added
       console.log("Time's up! Round over.");
-
     }
-  },1000);
-
+  }, 1000);
 }
 
 // Starts the timer
-startTimer(); 
+startTimer();
 
 let score = 0;
-// Score Implementation 
-function updateScore(points){
+// Score Implementation
+function updateScore(points) {
   score += points;
-  // May need to be altered when HTML is added 
-  console.log('Score: $ {score}');
-
+  // May need to be altered when HTML is added
+  console.log("Score: $ {score}");
 }
-
 
 let visibleCards = [];
 
 // Function to deal cards for initial 12 cards
 
-function dealCards(){
-  for(let i = 0; i < 12; i++){
-    visibleCards.forEach(cards.pop());
+function dealCards() {
+  for (let i = 1; i <= 12; i++) {
+    var card = cards.pop();
+    console.log(card.imgname());
+    visibleCards.push(card);
+    console.log("../img/" + card.imgname() + ".png");
+    document.getElementById("card-" + i).innerHTML =
+      `
+      <img class="card-img" src="../img/` +
+      card.imgname() +
+      `.png" />`;
   }
   console.log("Dealt Initial cards: ");
-  visibleCards.forEach((card,index) => console.log('${index + 1}: ${card.imgname()}'));
-
+  visibleCards.forEach((card, index) =>
+    console.log("${index + 1}: ${card.imgname()}")
+  );
 }
 
 // Function to replace the identified set with new cards
 
-function replaceCards(card1, card2, card3){
+function replaceCards(card1, card2, card3) {
   // Remove the three cards that form a valid set from visiblecards
-  visibleCards = visibleCards.filter(card => card !== card1 && card !== card2 && card !== card3);
+  visibleCards = visibleCards.filter(
+    (card) => card !== card1 && card !== card2 && card !== card3
+  );
 
   // Add three new cards from the deck if available
-  for(let i = 0; i < 3; i++){
-    if(cards.length > 0){
+  for (let i = 0; i < 3; i++) {
+    if (cards.length > 0) {
       visibleCards.push(cards.pop());
     }
   }
 
   console.log("New cards dealt after set identified: ");
-  visibleCards.forEach((card,index) => console.log('${index + 1}: ${card.imgname()}'));
-
+  visibleCards.forEach((card, index) =>
+    console.log("${index + 1}: ${card.imgname()}")
+  );
 }
 
-// Checks if selected cards form a set and updates the score 
-function checkAndUpdate(card1,card2,card3){
-  if (isSet(card1,card2,card3)){
+// Checks if selected cards form a set and updates the score
+function checkAndUpdate(card1, card2, card3) {
+  if (isSet(card1, card2, card3)) {
     updateScore(1);
     console.log("Correct set! Score increased. ");
-    replaceCards(card1,card2,card3);
-
-  }else{
+    replaceCards(card1, card2, card3);
+  } else {
     updateScore(-1);
     console.log("Incorrect set. Score decreased. ");
   }
